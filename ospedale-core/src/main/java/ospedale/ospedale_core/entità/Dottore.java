6dccfc480aa0ed903a31d5b4bf0ef8898ebb8b96
fede <federico.mazzetti.2@gmail.com>
2025-10-02 18:02:31 +0200

    Aggiunte 2 entità, cambiato ambiente Java

---
+++ ospedale-core/src/main/java/ospedale/ospedale_core/entità/Dottore.java
@@ -0,0 +1,27 @@
+package ospedale.ospedale_core.entità;
+
+public class Dottore {
+	    private Long id;
+
+	    private String nome;
+	    private String cognome;
+	    private String email;
+	    private String telefono;
+	    private String competenza;
+
+	    public Dottore() {}
+
+	    public Long getId() { return id; }
+	    public void setId(Long id) { this.id = id; }
+	    public String getNome() { return nome; }
+	    public void setNome(String nome) { this.nome = nome; }
+	    public String getCognome() { return cognome; }
+	    public void setCognome(String cognome) { this.cognome = cognome; }
+	    public String getEmail() { return email; }
+	    public void setEmail(String email) { this.email = email; }
+	    public String getTelefono() { return telefono; }
+	    public void setTelefono(String telefono) { this.telefono = telefono; }
+	    public String getSpecializzazione() { return competenza; }
+	    public void setSpecializzazione(String competenza) { this.competenza = competenza; }	
+
+}
--- project/src/main/java/ospedale/project/App.java
+++ project/src/main/java/ospedale/project/App.java
@@ -1,4 +1,4 @@
-package ospedale.ospedale_core;
+package ospedale.project;
 
 /**
  * Hello world!
