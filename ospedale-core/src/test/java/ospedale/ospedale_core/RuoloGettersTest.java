3cd834a2ac2ac8e237c5efff628e17b0c2b629ad
fede <federico.mazzetti.2@gmail.com>
2025-10-07 10:03:57 +0200

    Aggiunto test ruolo

---
+++ ospedale-core/src/test/java/ospedale/ospedale_core/RuoloGettersTest.java
@@ -0,0 +1,14 @@
+package ospedale.ospedale_core;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+
+import ospedale.ospedale_core.entità.Ruolo;
+
+class RuoloIdGetterTest {
+
+    @Test
+    void getId_coversGetterEvenIfNull() {
+    	Ruolo ruolo = new Ruolo("ROLE_USER");
+        assertNull(ruolo.getId());
+    }
+}
