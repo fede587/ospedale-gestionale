342783427f93bf92760e8fe25ecf1f17886ea10a
fede <federico.mazzetti.2@gmail.com>
2025-10-08 15:26:44 +0200

    fix DottoreService id

--- ospedale-core/src/main/java/ospedale/ospedale_core/service/DottoreService.java
+++ ospedale-core/src/main/java/ospedale/ospedale_core/service/DottoreService.java
@@ -1,9 +1,13 @@
 package ospedale.ospedale_core.service;
 
 import java.util.List;
+
 import ospedale.ospedale_core.entità.Dottore;
 
 interface DottoreService {
     List<Dottore> findAll();
+    <Dottore> void findById(Long id);
     Dottore save(Dottore m);
+    void deleteById(Long id);
+
 }
