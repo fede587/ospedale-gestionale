a620978ef446411a9b82e3a2786ed0a7fd81f8da
fede <federico.mazzetti.2@gmail.com>
2025-10-08 14:19:10 +0200

    Repository dottore

---
+++ ospedale-core/src/main/java/ospedale/ospedale_core/repository/DottoreRepository.java
@@ -0,0 +1,17 @@
+package ospedale.ospedale_core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ospedale.ospedale_core.entità.Dottore;
+import ospedale.ospedale_core.entità.Reparto;
+
+import java.util.List;
+
+public interface DottoreRepository extends JpaRepository<Dottore, Long> {
+
+  
+    List<Dottore> findByRepartoId(Long repartoId);
+
+  
+    List<Dottore> findByReparto(Reparto reparto);
+}
