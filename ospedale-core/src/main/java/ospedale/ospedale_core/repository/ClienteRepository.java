7cec20c80f07981fbecd035b20af579f2d0f0b98
fede <federico.mazzetti.2@gmail.com>
2025-10-08 14:33:51 +0200

    fix blank commit

--- ospedale-core/src/main/java/ospedale/ospedale_core/repository/ClienteRepository.java
+++ ospedale-core/src/main/java/ospedale/ospedale_core/repository/ClienteRepository.java
@@ -1,5 +1,10 @@
 package ospedale.ospedale_core.repository;
 
-public class ClienteRepository {
+import ospedale.ospedale_core.entità.Cliente;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByUsername(String username);
 }
