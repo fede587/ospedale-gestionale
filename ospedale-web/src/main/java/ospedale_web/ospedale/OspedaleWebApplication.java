6a40c7c78d18b0dfda1de698250a12223722cfa6
fede <federico.mazzetti.2@gmail.com>
2025-10-08 16:49:09 +0200

    Aggiunta parte web Maven, implementato OspedaleWebApplication

---
+++ ospedale-web/src/main/java/ospedale_web/ospedale/OspedaleWebApplication.java
@@ -0,0 +1,11 @@
+package ospedale_web.ospedale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspedaleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OspedaleWebApplication.class, args);
+    }
+}
