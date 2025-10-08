4ac5dcf862ad21a0d8f690d53c18cb0396b98bf7
fede <federico.mazzetti.2@gmail.com>
2025-10-08 16:52:46 +0200

    WebApplication aggiunti scan di ospedale entità

--- ospedale-web/src/main/java/ospedale_web/ospedale/OspedaleWebApplication.java
+++ ospedale-web/src/main/java/ospedale_web/ospedale/OspedaleWebApplication.java
@@ -2,8 +2,12 @@ package ospedale_web.ospedale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ospedale.ospedale")
+@EntityScan(basePackages = "ospedale.ospedale_core.entità")
 public class OspedaleWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(OspedaleWebApplication.class, args);
