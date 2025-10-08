deb76bd96173337f7b516246628ca9821609e0e9
fede <federico.mazzetti.2@gmail.com>
2025-10-08 14:36:55 +0200

    Ruolo Repository completo

--- ospedale-core/src/main/java/ospedale/ospedale_core/repository/RuoloRepository.java
+++ ospedale-core/src/main/java/ospedale/ospedale_core/repository/RuoloRepository.java
@@ -1,5 +1,10 @@
 package ospedale.ospedale_core.repository;
 
-public interface RuoloRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ospedale.ospedale_core.entità.Ruolo;
 
+import java.util.Optional;
+
+public interface RuoloRepository extends JpaRepository<Ruolo, Long> {
+    Optional<Ruolo> findByNome(String nome);
 }
