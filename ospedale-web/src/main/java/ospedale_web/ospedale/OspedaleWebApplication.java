e0819ec81b3c193fc7b34295bf43fc5a687c9fa0
fede <federico.mazzetti.2@gmail.com>
2025-10-09 14:57:00 +0200

    updated pom

--- ospedale-web/src/main/java/ospedale_web/ospedale/OspedaleWebApplication.java
+++ ospedale-web/src/main/java/ospedale_web/ospedale/OspedaleWebApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "ospedale.ospedale")
 @EntityScan(basePackages = "ospedale.ospedale_core.entità")
+@EnableJpaRepositories(basePackages = "ospedale.ospedale_core.repository")
+
 public class OspedaleWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(OspedaleWebApplication.class, args);
