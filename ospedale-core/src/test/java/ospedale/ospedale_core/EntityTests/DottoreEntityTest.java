d825c51150df35d86746ae95adfc8070c8147283
fede <federico.mazzetti.2@gmail.com>
2025-10-17 17:04:03 +0200

    Reparto tests in DottoreEntityTest, fixed assertEquals test

--- ospedale-core/src/test/java/ospedale/ospedale_core/EntityTests/DottoreEntityTest.java
+++ ospedale-core/src/test/java/ospedale/ospedale_core/EntityTests/DottoreEntityTest.java
@@ -1,4 +1,5 @@
 package ospedale.ospedale_core.EntityTests;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -8,42 +9,41 @@ import ospedale.ospedale_core.entità.Reparto;
 
 class DottoreEntityTest {
 
-    @Test
-    void gettersSettersTestDottore() {
-        Reparto reparto = new Reparto();
-        reparto.setNome("Cardio");
+	@Test
+	void gettersSettersTestDottore() {
+		Reparto reparto = new Reparto();
+		reparto.setNome("Cardio");
 
-        Dottore dr1 = new Dottore();
-        dr1.setNome("Mario");
-        dr1.setCognome("Rossi");
-        dr1.setEmail("mario.rossi@example.com");
-        dr1.setTelefono("0123");
-        dr1.setSpecializzazione("Cardiologia");
+		Dottore dr1 = new Dottore();
+		dr1.setNome("Mario");
+		dr1.setCognome("Rossi");
+		dr1.setEmail("mario.rossi@example.com");
+		dr1.setTelefono("0123");
+		dr1.setSpecializzazione("Cardiologia");
+		dr1.setReparto(reparto);
 
-        Dottore dr2 = new Dottore();
-        dr2.setNome("Mario");
-        dr2.setCognome("Rossi");
-        dr2.setEmail("mario.rossi@example.com");
-        dr2.setTelefono("0123");
-        dr2.setSpecializzazione("Cardiologia");
+		Dottore dr2 = new Dottore();
+		dr2.setNome("Mario");
+		dr2.setCognome("Rossi");
+		dr2.setEmail("mario.rossi@example.com");
+		dr2.setTelefono("0123");
+		dr2.setSpecializzazione("Cardiologia");
+		dr2.setReparto(reparto);
 
-     
-        assertEquals("Mario", dr2.getNome());
-        assertEquals("Rossi", dr2.getCognome());
-        assertEquals("mario.rossi@example.com", dr2.getEmail());
-        assertEquals("0123", dr2.getTelefono());
-        assertEquals("Cardiologia", dr2.getSpecializzazione());
+		assertEquals("Mario", dr1.getNome());
+		assertEquals("Rossi", dr1.getCognome());
+		assertEquals("mario.rossi@example.com", dr1.getEmail());
+		assertEquals("0123", dr1.getTelefono());
+		assertEquals("Cardiologia", dr1.getSpecializzazione());
+		assertEquals(reparto, dr1.getReparto());
 
-  
-        assertTrue(dr1.equals(dr1));
-        assertFalse(dr1.equals(null));
-        assertFalse(dr1.equals(new Object()));
+		assertTrue(dr1.equals(dr1));
+		assertFalse(dr1.equals(null));
+		assertFalse(dr1.equals(new Object()));
 
-   
-        dr1.equals(dr2);
+		dr1.equals(dr2);
 
-      
-        dr1.hashCode();
-        assertNotNull(dr1.toString());
-    }
+		dr1.hashCode();
+		assertNotNull(dr1.toString());
+	}
 }
