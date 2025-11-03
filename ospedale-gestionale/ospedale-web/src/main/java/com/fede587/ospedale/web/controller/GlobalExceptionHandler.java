2db1138654eb1862f73103827fdcd15fc145a0af
fede <federico.mazzetti.2@gmail.com>
2025-11-03 15:07:51 +0100

    added GlobalExceptionHandler

---
+++ ospedale-gestionale/ospedale-web/src/main/java/com/fede587/ospedale/web/controller/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.fede587.ospedale.web.controller;
+
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElement(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalState(IllegalStateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
