c6495217d25cbce952dfb48cb345fba4d12c794e
fede <federico.mazzetti.2@gmail.com>
2025-11-04 14:53:23 +0100

    Added simple Smoke Spring test

---
+++ ospedale-gestionale/ospedale-web/src/test/java/com/fede587/ospedale/web/SmokeIT.java
@@ -0,0 +1,13 @@
+package com.fede587.ospedale.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = OspedaleWebApplication.class)
+class SmokeIT {
+
+  @Test
+  void contextLoads() {
+    // SE IL SISTEMA SI AVVIA E NON DA ERRORI ALLORA IL TEST E' POSITIVO.
+  }
+}
