c00666eee592651ba8062525c1c95bcd9fdc1aed
fede <federico.mazzetti.2@gmail.com>
2025-11-05 16:45:08 +0100

    Specified Configuration SpringBoot in RepartoRepository

--- ospedale-gestionale/ospedale-core/src/test/java/com/fede587/ospedale/core/RepartoRepositoryTest.java
+++ ospedale-gestionale/ospedale-core/src/test/java/com/fede587/ospedale/core/RepartoRepositoryTest.java
@@ -6,7 +6,10 @@ import com.fede587.ospedale.core.repository.RepartoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,14 +18,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @org.springframework.test.context.ContextConfiguration(classes = RepartoRepositoryTest.class)
 
 @SpringBootConfiguration
+@EnableAutoConfiguration
+@Configuration
+@EntityScan(basePackages = "com.fede587.ospedale.core.entity")
+
 class RepartoRepositoryTest {
 
-    @Autowired RepartoRepository repo;
+	@Autowired
+	RepartoRepository repo;
 
-    @Test
-    void saveAndFindByNomeIgnoreCase() {
-        Reparto r = new Reparto("Cardiologia", "Cuore");
-        repo.save(r);
-        assertThat(repo.findByNomeIgnoreCase("cardiologia")).isPresent();
-    }
+	@Test
+	void saveAndFindByNomeIgnoreCase() {
+		Reparto r = new Reparto("Cardiologia", "Cuore");
+		repo.save(r);
+		assertThat(repo.findByNomeIgnoreCase("cardiologia")).isPresent();
+	}
 }
