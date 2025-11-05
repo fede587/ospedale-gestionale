2d44ef41593603c36ee9d00b2b8c2519118aba4a
fede <federico.mazzetti.2@gmail.com>
2025-11-05 14:25:16 +0100

    Added passwordEncoder to SecurityConfig

--- ospedale-gestionale/ospedale-web/src/main/java/com/fede587/ospedale/web/config/SecurityConfig.java
+++ ospedale-gestionale/ospedale-web/src/main/java/com/fede587/ospedale/web/config/SecurityConfig.java
@@ -3,18 +3,24 @@ package com.fede587.ospedale.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
   @Bean
+  PasswordEncoder passwordEncoder(){ return new BCryptPasswordEncoder(); }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
       .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/", "/login", "/registrazione").permitAll()
+        .requestMatchers("/", "/login", "/registrazione", "/h2-console/**").permitAll()
         .anyRequest().authenticated()
       )
+      .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
       .headers(h -> h.frameOptions(f -> f.sameOrigin()))
       .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/", true).permitAll())
       .logout(logout -> logout.logoutSuccessUrl("/login?logout").permitAll());
