a900edc00c7015448151f55d4ac76cc35391f831
fede <federico.mazzetti.2@gmail.com>
2025-11-06 14:47:57 +0100

    GlobalExceptionHandler 404 Test

---
+++ ospedale-gestionale/ospedale-web/src/test/java/com/fede587/ospedale/web/controller/GlobalExceptionHandlerTest.java
@@ -0,0 +1,19 @@
+package com.fede587.ospedale.web.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+class GlobalExceptionHandlerTest {
+
+    @Test
+    void handleNoSuchElement_returns404() {
+        GlobalExceptionHandler h = new GlobalExceptionHandler();
+        ResponseEntity<String> r = h.handleNoSuchElement(new NoSuchElementException("x"));
+        assertEquals(404, r.getStatusCode().value());
+        assertEquals("Not found", r.getBody());
+    }
+}
