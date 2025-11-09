9daab796f3ce73dd1054de62ed765e1404918f3f
fede <federico.mazzetti.2@gmail.com>
2025-11-09 16:33:57 +0100

    fix Reparto, invoked Jakarta.persistence

--- ospedale-gestionale/ospedale-core/src/main/java/com/fede587/ospedale/core/entity/Reparto.java
+++ ospedale-gestionale/ospedale-core/src/main/java/com/fede587/ospedale/core/entity/Reparto.java
@@ -3,12 +3,31 @@ package com.fede587.ospedale.core.entity;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "reparti", uniqueConstraints = @UniqueConstraint(columnNames = "nome"))
+
 public class Reparto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
 
 	private String nome;
+	@NotBlank
+	@Column(nullable = false)
 
 	private String descrizione;
+	@OneToMany(mappedBy = "reparto")
 
 	private Set<Dottore> dottori = new LinkedHashSet<>();
 
